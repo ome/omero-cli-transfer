009504b7874c6125f01fdb0310ff7bae9d04710f
jean-marie burel <j.burel@dundee.ac.uk>
2024-01-23 19:50:45 +0000

    Update infra (#75)
    * bump actions/checkout to v4
    
    * use branch
    
    * remove local copy of files from omero-test-infra
    
    * rename folder
    
    * exclude .DS_Store
    
    * change permissions
    
    * split test
    
    * do no upack if plate or screen
    
    * using submodules
    
    * set python version
    
    * bump actions/checkout to v4
    
    * use ome branch
    
    * bumping python to 3.9, new omero-test-infra
    
    * run the tests for plate and screen
    
    * use own branch
    
    * pass parameter
    
    * remove customised file
    
    * combine tests
    
    * use ome repository
    
    ---------
    
    Co-authored-by: Erick Martins Ratamero <erickmartins@gmx.net>

--- test/integration/test_transfer.py
+++ test/integration/test_transfer.py
@@ -391,32 +391,24 @@ class TestTransfer(CLITest):
         assert len(pl_ids) == 4
         self.delete_all()
 
+    @pytest.mark.parametrize('packing', ["tar", "zip"])
     @pytest.mark.parametrize('target_name', sorted(SUPPORTED))
-    def test_pack_unpack(self, target_name, tmpdir):
+    def test_pack_unpack(self, target_name, packing, tmpdir):
         if target_name == "datasetid" or target_name == "projectid" or\
            target_name == "idonly" or target_name == "imageid":
             self.create_image(target_name=target_name)
         elif target_name == "plateid" or target_name == "screenid":
             self.create_plate(plates=1, target_name=target_name)
         target = getattr(self, target_name)
-        args = self.args + ["pack", target, str(tmpdir / 'test.tar')]
+        if packing == "tar":
+            name = 'test.tar'
+            args = self.args + ["pack", target, str(tmpdir / name)]
+        else:
+            name = 'test.zip'
+            args = self.args + ["pack", target, "--zip", str(tmpdir / name)]
         self.cli.invoke(args, strict=True)
         self.delete_all()
-        args = self.args + ["unpack", str(tmpdir / 'test.tar')]
-        self.cli.invoke(args, strict=True)
-        self.run_asserts(target_name)
-        self.delete_all()
-
-        if target_name == "datasetid" or target_name == "projectid" or\
-           target_name == "idonly" or target_name == "imageid":
-            self.create_image(target_name=target_name)
-        elif target_name == "plateid" or target_name == "screenid":
-            self.create_plate(plates=1, target_name=target_name)
-        target = getattr(self, target_name)
-        args = self.args + ["pack", target, "--zip", str(tmpdir / 'test.zip')]
-        self.cli.invoke(args, strict=True)
-        self.delete_all()
-        args = self.args + ["unpack", str(tmpdir / 'test.zip')]
+        args = self.args + ["unpack", str(tmpdir / name)]
         self.cli.invoke(args, strict=True)
         self.run_asserts(target_name)
         self.delete_all()
