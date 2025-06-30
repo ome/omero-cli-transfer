abdaf5049eb6fac587249a1318e681453d491fb6
Erick Martins Ratamero <erickmartins@gmx.net>
2025-06-30 15:44:29 -0400

    removing cruft from setup.py

--- setup.py
+++ setup.py
@@ -10,30 +10,3 @@ import os
 setuptools.setup(
     version=os.environ.get('VERSION', '0.0.0'),
 )
-
-
-
-setup(
-    packages=['', 'omero.plugins'],
-    package_dir={"": "src"},
-    name="omero-cli-transfer",
-    version='1.2.0',
-    maintainer="Erick Ratamero",
-    maintainer_email="erick.ratamero@jax.org",
-    description=("A set of utilities for exporting a transfer"
-                 " packet from an OMERO server and importing "
-                 "it in a different server. Developed by the "
-                 "Research IT team at The Jackson Laboratory."),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/TheJacksonLaboratory/omero-cli-transfer",
-    install_requires=[
-        'ezomero>=3.1.0, <4.0.0',
-        'ome-types>=0.6.1, <0.7.0'
-    ],
-    extras_require={
-        "rocrate": ["rocrate>=0.7.0, <1.0.0"],
-    },
-    python_requires='>=3.9',
-
-)
