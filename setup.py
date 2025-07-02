72d7afcedd2bbaf33fb392a88af3d5126fb21295
Erick Martins Ratamero <erickmartins@gmx.net>
2025-07-02 12:38:45 -0400

    Use `pyproject.toml` instead of `setup.py` (#106)
    * first pass at pyproject.toml
    
    * removing cruft from setup.py
    
    * removing unnecessary setuptools legacy stuff
    
    * using dynamic version from GH release tag on PyPI builds

--- setup.py
+++ setup.py
@@ -4,42 +4,9 @@
 #
 # Use is subject to license terms supplied in LICENSE.
 
+import setuptools
 import os
-from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
-def read(fname):
-    """
-    Utility function to read the README file.
-    :rtype : String
-    """
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    packages=['', 'omero.plugins'],
-    package_dir={"": "src"},
-    name="omero-cli-transfer",
-    version='1.2.0',
-    maintainer="Erick Ratamero",
-    maintainer_email="erick.ratamero@jax.org",
-    description=("A set of utilities for exporting a transfer"
-                 " packet from an OMERO server and importing "
-                 "it in a different server. Developed by the "
-                 "Research IT team at The Jackson Laboratory."),
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/TheJacksonLaboratory/omero-cli-transfer",
-    install_requires=[
-        'ezomero>=3.1.0, <4.0.0',
-        'ome-types>=0.6.1, <0.7.0'
-    ],
-    extras_require={
-        "rocrate": ["rocrate>=0.7.0, <1.0.0"],
-    },
-    python_requires='>=3.9',
-
+setuptools.setup(
+    version=os.environ.get('VERSION', '0.0.0'),
 )
